804ddafdf132a65374e3ec0588a1d2cc20a1282f
Miljan Aleksic <miljan@aleksic.es>
2017-10-08 21:51:58 +0800

    add write log option

--- packages/write/index.js
+++ packages/write/index.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
 const path = require('path')
+const chalk = require('chalk')
 const mkdirp = require('mkdirp')
 
-module.exports = function (dest, data) {
+module.exports = function (dest, data, options) {
   return new Promise(async (resolve, reject) => {
 
     try {
@@ -12,6 +13,26 @@ module.exports = function (dest, data) {
       reject(e)
     }
 
+    if (options.log) {
+      const stat = await fs.statSync(dest)
+      const size = formatBytes(stat.size)
+
+      log(`${dest} - ${size}`)
+    }
+
     resolve(dest)
   })
+}
+
+function log (msg) {
+  console.log(chalk.blue(msg))
+}
+
+function formatBytes (bytes, decimals = 2) {
+  if (bytes === 0) return '0 Bytes'
+  const k = 1000
+  const dm = decimals || 2
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  let i = Math.floor(Math.log(bytes) / Math.log(k))
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
 }
