68989fdacf293a72df2db0332e46c9277d29c9af
Miljan Aleksic <miljan@aleksic.es>
2017-11-13 20:47:47 +0800

    update @less
    - add minify option
    - add sourceMap option
    - add log option

--- packages/less/index.js
+++ packages/less/index.js
@@ -1,26 +1,50 @@
-const lessCompiler = require('less')
+const path = require('path')
+const less = require('less')
 const read = require('@lump/read')
 const write = require('@lump/write')
+const LessPluginCleanCSS = require('less-plugin-clean-css')
+
+const defaults = {
+  log: false,
+  minify: false,
+  sourceMap: false
+  // ... less render options
+}
 
 /*
  * Compile less from file/less to file/css
  */
-module.exports = async ({ src, content, dest, options = {} }) => {
+module.exports = async ({ src, content, dest, options = defaults }) => {
   if (!src && !content) {
     throw new Error('Missing Arguments')
   }
 
-  const less = src
+  // get less source
+  const source = src
     ? await read(src)
     : content
-  const css = await renderLess(less, options)
 
-  return dest
-    ? write(dest, css)
-    : css
-}
+  // set options
+  if (options.sourceMap) {
+    options.sourceMap = {
+      sourceMapURL: `${path.basename(dest)}.map`
+    }
+  }
 
-function renderLess (data, options) {
-  return lessCompiler.render(data, options)
-    .then(output => output.css, error => console.log(error))
+  if (options.minify) {
+    options.plugins = [ new LessPluginCleanCSS() ]
+  }
+
+  // render
+  const output = await less.render(source, options)
+
+  if (dest) {
+    await write(dest, output.css, { log: options.log })
+  }
+
+  if (dest && output.map) {
+    await write(`${dest}.map`, output.map, { log: options.log })
+  }
+
+  return output.css
 }
